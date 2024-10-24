e6b9625fb06c877dcf6f646ee60d5a3cf91c23b8
sreyas <146116284+sreyas-b-anand@users.noreply.github.com>
2024-10-24 23:00:49 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -21,8 +21,33 @@ string name1, name2;
 char player;
 int m = 0;
 
-int main()
-{
+void clearBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            ar[i][j] = "[   ]";
+        }
+    }
+    m = 0;
+}
+
+bool getPlayerInput(int &i, int &j, int playerNum) {
+    while (true) {
+        cout << "Player " << playerNum << " to play\n";
+        cout << "Input the position (row and column): ";
+        cin >> i >> j;
+
+        i--;
+        j--; 
+
+        if (ar[i][j] != player1 && ar[i][j] != player2) {
+            return true; 
+        } else {
+            cout << BLUE << "Error: Position already taken. Try again." << RESET << endl;
+        }
+    }
+}
+
+int main() {
     int i, j;
     bool winner = false;
 
@@ -35,110 +60,58 @@ int main()
     cout << "\nChoose either X or O: ";
     cin >> player;
 
-    if (player == 'X' || player == 'x')
-    {
+    if (player == 'X' || player == 'x') {
         player1 = "[ X ]";
         player2 = "[ O ]";
-    }
-    else if (player == 'O' || player == 'o')
-    {
+    } else if (player == 'O' || player == 'o') {
         player1 = "[ O ]";
         player2 = "[ X ]";
-    }
-    else
-    {
+    } else {
         cout << "Error: Invalid choice. Exiting...\n\n";
+        return 1;
     }
 
-    cout << "\n\n"
-         << name1 << " is player 1 and plays with " << player1;
-    cout << "\n"
-         << name2 << " is player 2 and plays with " << player2;
+    cout << "\n\n" << name1 << " is player 1 and plays with " << player1;
+    cout << "\n" << name2 << " is player 2 and plays with " << player2;
 
     cout << "\n\nInstructions";
-    cout << CYAN << "\n\nTo input the character in position a22,\n\t" << "Enter 2 2\n"
-         << RESET;
-    cout << "\n\nSimilarly,input the required positions throughout game \n\n ";
+    cout << CYAN << "\n\nTo input the character in position a22,\n\t" << "Enter 2 2\n" << RESET;
+    cout << "\n\nSimilarly, input the required positions throughout the game \n\n ";
 
-    cout << CYAN "\n\nCURRENT STATE and POSITIONS\n"
-         << RESET;
+    cout << CYAN << "\n\nCURRENT STATE and POSITIONS\n" << RESET;
     cout << "\n[a11] [a12] [a13]\n"
-            "[a21] [a22] [a23]\n"
-            "[a31] [a32] [a33] \n\n";
+         << "[a21] [a22] [a23]\n"
+         << "[a31] [a32] [a33] \n\n";
 
-    cout << CYAN << "\n\nLET THE GAME BEGINS......\n"
-         << RESET;
+    cout << CYAN << "\n\nLET THE GAME BEGIN......\n" << RESET;
 
-    while (!winner)
-    {
-        // Player 1
-        do
-        {
-            cout << "\nPlayer 1 to play\n";
-            cout << "Input the position : ";
-            cin >> i >> j;
-            i--;
-            j--;
-            if (i >= 0 && i < 3 && j >= 0 && j < 3 && ar[i][j] != player1 && ar[i][j] != player2)
-            {
-                ar[i][j] = player1;
-                m++;
-                break;
-            }
-            else
-            {
-                cout << BLUE << "\nError: Invalid position..... Try again.\n"
-                     << RESET;
-            }
-        } while (true);
-
+    while (!winner) {
+        // Player 1's turn
+        if (getPlayerInput(i, j, 1)) {
+            ar[i][j] = player1;
+            m++;
+        }
         currentState();
-
-        if (conditionChecker())
-        {
-            cout << YELLOW << "\n"
-                 << name1 << " is the winner!" << endl
-                 << RESET;
+        if (conditionChecker()) {
+            cout << YELLOW << "\n" << name1 << " is the winner!" << endl << RESET;
             cout << "\nWELL PLAYED\n";
             winner = true;
             break;
         }
-        if (m == 9)
-        {
+        if (m == 9) {
             cout << YELLOW << "\nThe game is drawn" << RESET << endl;
-            cout << "\nWELL PLAYED"
-                 << endl;
+            cout << "\nWELL PLAYED" << endl;
             break;
         }
 
-        // Player 2
-        do
-        {
-            cout << "\nPlayer 2 to play\n";
-            cout << "Input the position : ";
-            cin >> i >> j;
-            i--;
-            j--;
-            if (i >= 0 && i < 3 && j >= 0 && j < 3 && ar[i][j] != player1 && ar[i][j] != player2)
-            {
-                ar[i][j] = player2;
-                m++;
-                break;
-            }
-            else
-            {
-                cout << BLUE "\nError: Invalid position..... Try again.\n"
-                     << RESET;
-            }
-        } while (true);
-
+        // Player 2's turn
+        if (getPlayerInput(i, j, 2)) {
+            ar[i][j] = player2;
+            m++;
+        }
         currentState();
-
-        if (conditionChecker())
-        {
-            cout << YELLOW << "\n"
-                 << name2 << " is the winner!" << endl
-                 << RESET;
+        if (conditionChecker()) {
+            cout << YELLOW << "\n" << name2 << " is the winner!" << endl << RESET;
             cout << "\nWELL PLAYED\n";
             winner = true;
             break;
@@ -148,41 +121,32 @@ int main()
     return 0;
 }
 
-bool conditionChecker()
-{
-    for (int i = 0; i < 3; ++i)
-    {
-        if (ar[i][0] == ar[i][1] && ar[i][1] == ar[i][2] && ar[i][0] != "[   ]")
-        {
+bool conditionChecker() {
+    for (int i = 0; i < 3; ++i) {
+        if (ar[i][0] == ar[i][1] && ar[i][1] == ar[i][2] && ar[i][0] != "[   ]") {
             return true;
         }
-        if (ar[0][i] == ar[1][i] && ar[1][i] == ar[2][i] && ar[0][i] != "[   ]")
-        {
+        if (ar[0][i] == ar[1][i] && ar[1][i] == ar[2][i] && ar[0][i] != "[   ]") {
             return true;
         }
     }
-    if (ar[0][0] == ar[1][1] && ar[1][1] == ar[2][2] && ar[0][0] != "[   ]")
-    {
+    if (ar[0][0] == ar[1][1] && ar[1][1] == ar[2][2] && ar[0][0] != "[   ]") {
         return true;
     }
-    if (ar[0][2] == ar[1][1] && ar[1][1] == ar[2][0] && ar[0][2] != "[   ]")
-    {
+    if (ar[0][2] == ar[1][1] && ar[1][1] == ar[2][0] && ar[0][2] != "[   ]") {
         return true;
     }
     return false;
 }
 
-void currentState()
-{
-    cout << MAGENTA << "\nCurrent state \n"
-         << RESET;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+void currentState() {
+    cout << MAGENTA << "\nCurrent state \n" << RESET;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             cout << ar[i][j];
             cout << " ";
         }
         cout << endl;
     }
 }
+
